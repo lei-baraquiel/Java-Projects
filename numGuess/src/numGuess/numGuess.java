29ed51faccc9e7e25fa4b4afb2a48ec1402bdfab
furei19 <josefurei2019@gmail.com>
2025-09-02 23:04:15 +0800

    add 10 lives to numguess

--- numGuess/src/numGuess/numGuess.java
+++ numGuess/src/numGuess/numGuess.java
@@ -9,11 +9,10 @@ import java.util.Scanner;
 public class numGuess {
     public static void main(String[] args) {
         int randomNum = (int)(Math.random() * 100);
-        int num = 0;
+        int num = 0, lives = 10;
         
         Scanner scan = new Scanner(System.in);
-        
-        while(num != randomNum) {
+        while(lives > 0) {
             while(true) {
                 System.out.print("Input Number from 1-100: ");
                 if (scan.hasNextInt()) {
@@ -28,13 +27,22 @@ public class numGuess {
             
             if (num > randomNum) {
                 System.out.println("Too High!");
+                lives--;
             } else if (num < randomNum) {
                 System.out.println("Too Low!");
+                lives--;
             } else {
                 scan.close();
                 System.out.println("You Won!");
                 break;
             }
+            
+            if (lives == 0) {
+                System.out.println("You don't have any more lives!");
+                System.out.println("You Lost");
+            }
+            
+            System.out.println("You have " + lives + " Lives left");
         }
     }
 }
